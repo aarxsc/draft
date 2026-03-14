b7d5071366cb23f23c6c1c409bb43896fa4b7647
aarxsc <malacayathara7@gmail.com>
2026-03-14 17:04:08 +0800

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://blogastro1.com"
-});
-
+  site: "https://blogastro1.netlify.app",
+  integrations: [preact()]
+})
